e2448348c8a8b24a582fb3f1369b745997d96ce8
cosmo <cosmo8082@163.com>
2021-08-09 16:15:56 +0800

    2021-8-9 edit

--- injected/health_report.js
+++ injected/health_report.js
@@ -1,25 +1,27 @@
     window.addEventListener("contentMsg", function(data) {
         // alert(data.detail)
-        if(vm.hasFlag != "1"){
-            var sfzx = '1'; // 是否在校，1表示在校，0表示不在校
-            var province = '浙江', city = '杭州', district = '西湖'; // 修改你希望的所在地
-            vm.info.sfzx = sfzx;
-            vm.info.area = province + '省 ' + city + '市 ' + district + '区';
-            vm.info.address = province + '省' + city + '市' + district + '区';
-            vm.info.city = city + '市';
-            vm.info.province = province +'省';
-            vm.info.jrdqtlqk = ["0"];
-            vm.info.sfymqjczrj = 0;
-            vm.info.sfqrxxss = 1;
-            vm.save();
-            alert('打卡成功');
-            window.dispatchEvent(new CustomEvent("injectedMsg", {detail: '打卡成功'}));
-            setTimeout( function(){
-                window.close();
-            }, 1000);
-        }else{
-            alert('已经打过卡了');
-            window.dispatchEvent(new CustomEvent("injectedMsg", {detail: '已经打过卡了'}));
-            window.close();
-        }
     }, false);
+    if(vm.hasFlag != "1"){
+	    var sfzx = '1'; // 是否在校，1表示在校，0表示不在校
+	    var province = '浙江', city = '杭州', district = '西湖'; // 修改你希望的所在地
+	    vm.info.sfzx = sfzx;
+
+	    vm.info.area = province + '省 ' + city + '市 ' + district + '区';
+	    vm.info.address = province + '省' + city + '市' + district + '区';
+	    vm.info.city = city + '市';
+
+	    vm.info.province = province +'省';
+	    vm.info.jrdqtlqk = ["0"];
+	    vm.info.sfymqjczrj = 0;
+	    vm.info.sfqrxxss = 1;
+	    vm.save();
+	    // alert('打卡成功');
+	    // window.dispatchEvent(new CustomEvent("injectedMsg", {detail: '打卡成功'}));
+	    setTimeout( function(){
+		window.close();
+	    }, 1000);
+	}else{
+	    // alert('已经打过卡了');
+	    // window.dispatchEvent(new CustomEvent("injectedMsg", {detail: '已经打过卡了'}));
+	    window.close();
+	}
